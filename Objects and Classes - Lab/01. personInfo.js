160b1a1fda79e39c6130c189b64b29b4a7f061b6
Donika <121803093+DonikaMarcheva@users.noreply.github.com>
2023-02-20 22:45:46 +0200

    .

---
+++ Objects and Classes - Lab/01. personInfo.js
@@ -0,0 +1,11 @@
+function personalInfo(firstName, lastName, age){
+let obj={};
+obj.firstName=firstName;
+obj.lastName=lastName;
+obj.age=age;
+return obj;
+}
+personalInfo("Peter", 
+"Pan",
+"20"
+)
