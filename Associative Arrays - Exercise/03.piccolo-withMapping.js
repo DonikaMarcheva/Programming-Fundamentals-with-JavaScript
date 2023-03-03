76ae36b781889913bd37e6b1989969bff66ccb37
Donika <121803093+DonikaMarcheva@users.noreply.github.com>
2023-03-03 22:15:17 +0200

    Create 03.piccolo-withMapping.js

---
+++ Associative Arrays - Exercise/03.piccolo-withMapping.js
@@ -0,0 +1,34 @@
+function piccolo(input) {
+    let carLeft = new Map();
+    for (let line of input) {
+        let [direction, carNum] = line.split(', ');
+        if (direction === "IN") {
+            carLeft.set(carNum, "");
+        } else {
+            if (carLeft.has(carNum)) {
+                carLeft.delete(carNum)
+            }
+        }
+    }
+    if (carLeft.size === 0) {
+        console.log("Parking Lot is Empty");
+    } else {
+        let carLeftArr = Array.from(carLeft).sort((a, b) => a[0].localeCompare(b[0]));
+        for (let carNumber of carLeftArr) {
+            console.log(carNumber[0]);
+        }
+    }
+}
+
+
+piccolo(['IN, CA2844AA',
+    'IN, CA1234TA',
+    'OUT, CA2844AA',
+    'IN, CA9999TT',
+    'IN, CA2866HI',
+    'OUT, CA1234TA',
+    'IN, CA2844AA',
+    'OUT, CA2866HI',
+    'IN, CA9876HH',
+    'IN, CA2822UU']
+)
