c301aa46f1541b09006f1a4f007b354048f254c2
Donika <121803093+DonikaMarcheva@users.noreply.github.com>
2023-03-09 14:23:09 +0200

    replacing an empty file with the right one

---
+++ Text Processing - Lab/03.censoredWords.js
@@ -0,0 +1,7 @@
+function censored(sentence, word) {
+    while (sentence.includes(word)) {
+        sentence = sentence.replace(word, "*".repeat(word.length))
+    }
+    console.log(sentence)
+}
+censored('Find the hidden word', 'hidden')
