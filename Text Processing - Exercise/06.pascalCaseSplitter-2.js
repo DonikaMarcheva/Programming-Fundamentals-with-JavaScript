02720c06fcc6b4b608e810fda2f19de9a56d3ba3
Donika <121803093+DonikaMarcheva@users.noreply.github.com>
2023-03-10 14:45:31 +0200

    Create 06.pascalCaseSplitter-2.js

---
+++ Text Processing - Exercise/06.pascalCaseSplitter-2.js
@@ -0,0 +1,15 @@
+function caseSplitter(text) {
+    let result = [];
+    let upperIndex = 0;
+
+    for (let i = 1; i < text.length; i++) {
+        if (text[i] === text[i].toUpperCase()) {
+            let word = text.substring(upperIndex, i);
+            result.push(word);
+            upperIndex = i;
+        }
+    }
+    result.push(text.substring(upperIndex));
+    console.log(result.join(', '));
+}
+caseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan')
